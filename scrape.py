b2647da53f4c7f957bd197492a3cfe28563ba518
Peter Snyder <snyderp@gmail.com>
2018-05-17 05:56:05 +0000

    move back to process pool

--- scrape.py
+++ scrape.py
@@ -8,7 +8,7 @@ import sys
 import uuid
 import boto3
 import botocore
-from multiprocessing.pool import ThreadPool
+from multiprocessing import Pool
 
 LAMBDA_FUNC_NAME = "arn:aws:lambda:us-east-1:275005321946:function:brave-script-gatherer"
 COIN_TERMS = [
@@ -41,23 +41,23 @@ COIN_DOMAINS = [
 ]
 
 def target(subproc_args):
-    url, rank, num_domains, region, batch_tag, debug = subproc_args
-    scrape_args = {
-        "url": url,
-        "rank": rank,
-        "terms": COIN_TERMS,
-        "domains": COIN_DOMAINS,
-        "batch": batch_tag,
-        "region": region,
-    }
-
-    if debug:
-        scrape_args["debug"] = debug
-
-    config = botocore.client.Config(read_timeout=360, retries={'max_attempts': 0})
-    client = boto3.client('lambda', region_name='us-east-1', config=config)
-    print(u"{} / {}: Scraping {}".format(rank, num_domains, domain))
     try:
+        rank, url, region, batch_tag, debug = subproc_args
+        scrape_args = {
+            "url": "http://" + url,
+            "rank": rank,
+            "terms": COIN_TERMS,
+            "domains": COIN_DOMAINS,
+            "batch": batch_tag,
+            "region": region,
+        }
+
+        if debug:
+            scrape_args["debug"] = debug
+
+        config = botocore.client.Config(read_timeout=360, retries={'max_attempts': 0})
+        client = boto3.client('lambda', region_name='us-east-1', config=config)
+        print(u"{}: Scraping {}".format(rank, url))
         client.invoke(
             FunctionName=LAMBDA_FUNC_NAME,
             InvocationType="RequestResponse",
@@ -65,7 +65,7 @@ def target(subproc_args):
             Payload=json.dumps(scrape_args, ensure_ascii=False)
         )
     except Exception, e:
-        error_params = [domain, str(e)]
+        error_params = [url, str(e)]
         sys.stderr.write("Error scraping {}\n{}\n".format(*error_params))
 
 
@@ -84,13 +84,13 @@ if __name__ == "__main__":
                              "debugging information.")
     ARGS = PARSER.parse_args()
 
-    HANDLE = open(ARGS.domain_list, "r")
-    DOMAINS = HANDLE.read().strip().split("\n")
-    HANDLE.close()
     TAG = str(uuid.uuid4())
     CC = ARGS.country_code
     DEBUG = ARGS.debug
 
-    SUBPROCESS_ARGS = [(r + 1, d, len(DOMAINS), CC, TAG, DEBUG) for (r, d) in enumerate(DOMAINS)]
-    POOL = ThreadPool(processes=50)
-    POOL.map(target, SUBPROCESS_ARGS)
+    with open(ARGS.domain_list, "r") as handle:
+        SUBPROCESS_ARGS = ((r + 1, d.strip(), CC, TAG, DEBUG) for (r, d) in enumerate(handle))
+        POOL = Pool(processes=50)
+        POOL.map(target, SUBPROCESS_ARGS)
+        POOL.close()
+        POOL.join()
--- scrape.sh
+++ scrape.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 CUR_DIR=`dirname "$(readlink -f "$0")"`
 source "$CUR_DIR/../bin/activate";
-python $CUR_DIR/abp_alexa_scrape.py;
+ALEXA_LIST=$CUR_DIR/alexa-lists/alexa-1m.txt;
+python2.7 $CUR_DIR/scrape.py \
+    --domain-list $ALEXA_LIST;
